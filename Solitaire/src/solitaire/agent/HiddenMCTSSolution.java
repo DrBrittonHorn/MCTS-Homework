b34ce2e9add69cf5ad7d8eef014aa69dfe7724e7
DrHorn <bhorn@trinity.edu>
2021-04-30 15:05:05 -0500

    Added hidden info mcts implementation stub

---
+++ Solitaire/src/solitaire/agent/HiddenMCTSSolution.java
@@ -0,0 +1,23 @@
+package solitaire.agent;
+
+import java.util.Random;
+import solitaire.game.Game;
+import solitaire.game.Move;
+
+public class HiddenMCTSSolution extends Agent {
+	private Game game = null;
+	private static final Random rand = new Random();
+	Node root = null;
+	@Override
+	public Move getMove(Game game, long timeDue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private class Node
+	{
+		
+	
+	}
+
+}
